b459667e2b586754321aca43e727d7a20f7c926c
mannataneja <tt3092@g.rit.edu>
2025-11-11 21:54:53 -0500

    proj

---
+++ RacingGame/Source/RacingGame/APIMapCreator.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "APIMapCreator.h"
+
+#include "ArcGISMapsSDK/Actors/ArcGISActor.h"
+#include "ArcGISMapsSDK/Actors/ArcGISMapActor.h"
+#include "ArcGISMapsSDK/Components/ArcGISLocationComponent.h"
+#include "ArcGISMapsSDK/Components/ArcGISMapComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AAPIMapCreator::AAPIMapCreator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	locationComponent = CreateDefaultSubobject<UArcGISLocationComponent>("LocationComponent");
+	locationComponent->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void AAPIMapCreator::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	const auto mapComponentActor = UGameplayStatics::GetActorOfClass(GetWorld(), AArcGISMapActor::StaticClass());
+	mapComponent = Cast<AArcGISMapActor>(mapComponentActor)->GetMapComponent();
+}
+
+void AAPIMapCreator::SetOrigin(UArcGISPoint* NewPosition)
+{
+	if (!mapComponent)
+	{
+		return;
+	}
+	mapComponent->SetOriginPosition(NewPosition);
+}
+
+// Called every frame
+void AAPIMapCreator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
